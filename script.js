70f230101c11a105f0854305ae56f1436d615d86
hesham <heshammahmoud9733@gmail.com>
2022-01-04 16:48:49 +0200

    project_start_point

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container');
+const para = document.createElement('p');
+para.style.color = 'black';
+para.textContent = 'Second Paragraph';
+container.appendChild(para);
